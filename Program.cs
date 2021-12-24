cda77942e7801f4f9c8e75317e704b698eca44bf
MariiaDemidova <95860502+MariiaDemidova@users.noreply.github.com>
2021-12-24 18:47:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<int> tinkoff = new BankAccount <int> (2345, 125.214, "Демидова Мария");
+            BankAccount<string> sberbank = new BankAccount<string>("EEE254", 23.125, "Демидова Мария");
+            tinkoff.Print<int>(2345);
+            sberbank.Print<string>("EEE254");
+            Console.ReadKey();
+        }
+    }
+    class BankAccount <T>
+    {
+        public T Number { get; set; }
+        public double Balance { get; set; }
+        public string Name { get; set; }
+        public BankAccount(T number, double balance, string name)
+        {
+            Number = number;
+            Balance = balance;
+            Name = name;
+        }
+       public void Print <T> (T value)
+        {
+            Console.WriteLine("Счет номер {0}, владелец счета - {1}, сумма на счете - {2}", value, Name, Balance);
+        }
+    }
+}
